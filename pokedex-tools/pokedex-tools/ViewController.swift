686edd00a24554435c5e0dac9b16a903da52537a
Mike <mferro@uvm.edu>
2021-10-22 10:21:45 -0400

    Created Project Xcode folder

---
+++ pokedex-tools/pokedex-tools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokedex-tools
+//
+//  Created by user203780 on 10/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pokedex-tools/pokedex-toolsUITests/pokedex_toolsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  pokedex_toolsUITestsLaunchTests.swift
+//  pokedex-toolsUITests
+//
+//  Created by user203780 on 10/22/21.
+//
+
+import XCTest
+
+class pokedex_toolsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
